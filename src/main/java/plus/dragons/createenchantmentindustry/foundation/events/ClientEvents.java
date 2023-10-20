a8e1f4f7a44fa760a657a8d6f7c3d60b684b2608
Tai Chi Minh Ralph Eastwood <caffe@disroot.org>
2023-10-20 22:32:52 +0200

    Use PortingLib's events for fog color

--- src/main/java/plus/dragons/createenchantmentindustry/foundation/events/ClientEvents.java
+++ src/main/java/plus/dragons/createenchantmentindustry/foundation/events/ClientEvents.java
@@ -1,14 +1,65 @@
 package plus.dragons.createenchantmentindustry.foundation.events;
 
-//import plus.dragons.createdragonlib.event.TextureStitchPreEvent;
-import plus.dragons.createdragonlib.event.ViewPointFogColorEvent;
-import plus.dragons.createenchantmentindustry.content.contraptions.enchanting.enchanter.BlazeEnchanterRenderer;
-import plus.dragons.createenchantmentindustry.content.contraptions.fluids.ink.InkRenderingCamera;
+import com.mojang.blaze3d.shaders.FogShape;
+
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.FogType;
+import net.minecraft.world.level.Level;
+import net.minecraft.client.renderer.FogRenderer;
+
+import io.github.fabricators_of_create.porting_lib.event.client.FogEvents;
+import io.github.fabricators_of_create.porting_lib.event.client.FogEvents.ColorData;
+
+import plus.dragons.createenchantmentindustry.entry.CeiFluids;
+import plus.dragons.createenchantmentindustry.foundation.config.CeiConfigs;
 
 public class ClientEvents {
 	public static void register(){
-		ViewPointFogColorEvent.CallBack.EVENT.register(InkRenderingCamera::cameraEffect);
-		//fixme
-		//TextureStitchPreEvent.CallBack.EVENT.register(BlazeEnchanterRenderer::loadTexture);
+		FogEvents.RENDER_FOG.register(ClientEvents::getFogDensity);
+		FogEvents.SET_COLOR.register(ClientEvents::getFogColor);
+	}
+
+	public static boolean getFogDensity(FogRenderer.FogMode mode, FogType type, Camera camera, float partialTick, float renderDistance, float nearDistance, float farDistance, FogShape shape, FogEvents.FogData fogData) {
+		Level level = Minecraft.getInstance().level;
+		BlockPos blockPos = camera.getBlockPosition();
+		FluidState fluidState = level.getFluidState(blockPos);
+
+		if (camera.getPosition().y >= blockPos.getY() + fluidState.getHeight(level, blockPos))
+			return false;
+
+		Fluid fluid = fluidState.getType();
+		Entity entity = camera.getEntity();
+
+		if (CeiFluids.INK.get()
+			.isSame(fluid)) {
+			fogData.scaleFarPlaneDistance(1f / 8f * CeiConfigs.client().inkTransparencyMultiplier.getF());
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void getFogColor(ColorData event, float partialTicks) {
+		Camera info = event.getCamera();
+		Level level = Minecraft.getInstance().level;
+		BlockPos blockPos = info.getBlockPosition();
+		FluidState fluidState = level.getFluidState(blockPos);
+		if (info.getPosition().y > blockPos.getY() + fluidState.getHeight(level, blockPos))
+			return;
+
+		Fluid fluid = fluidState.getType();
+
+		if (CeiFluids.INK.get()
+			.isSame(fluid)) {
+			event.setRed(0 / 255f);
+			event.setGreen(0 / 255f);
+			event.setBlue(0 / 255f);
+			return;
+		}
 	}
 }
